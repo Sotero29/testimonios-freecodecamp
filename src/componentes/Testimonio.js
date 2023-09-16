037aa9dd651eb339130bda7e2513f1704815547c
Sotero29 <soteroarmentagarcia05@gmail.com>
2023-09-16 16:47:47 -0600

    Este es mi primer repositorio

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Testtimonio(){
+  return(
+    <div className='contenedor-testimonios'>
+      <img 
+      className='imagen-testimonio'
+      src={require('../imagenes/testimonio-Emma.png')}
+      alt='foto de Emma'/>
+    </div>
+
+  ); 
+}
